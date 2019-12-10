3eae44b2ba7583449cc218aa02c780cc2ff66b2f
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-12-10 00:07:49 +0000

    base for document selects

---
+++ src/js/views/base.js
@@ -0,0 +1,6 @@
+export const elements = {
+    searchRecipeImage: 'https://spoonacular.com/recipeImages/',
+    searchInput: document.querySelector('.search__field'),
+    searchForm: document.querySelector('.search'),
+    searchResultList: document.querySelector('.results__list'),
+}
