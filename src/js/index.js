b9623f789c59af3f86d00a1b9e647faff812adf3
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-12-11 05:04:15 +0000

    controller and eventListener for search

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,45 @@
-// Global app controller
-import num from './test';
-const x = 23;
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+/*
+Global state of the app 
+* Search object 
+* Current recipe object
+* Shopping list object
+* Liked recipes
+*/
+const state = {}
+
+const controlSearch = async () => {
+    // 1. Get query from the view
+    const query = searchView.getInput();
+    console.log(query);
+
+    if(query){
+        // 2. new search object and add to state
+        state.search = new Search(query);
+
+
+        // 3. prepare UI for results 
+        searchView.clearInput();
+        searchView.clearResults();
+
+
+        // 4. search for recipes 
+        // use await because promise is returned from async function getResults()
+        // so we await until promise resolves and comes back with data  
+        await state.search.getResults(); 
+
+
+        // 5. render results on UI
+        searchView.renderResults(state.search.recipes);
+        // console.log(state.search.recipes);
+    }
+    
+}
+
+elements.searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    controlSearch();
+});
