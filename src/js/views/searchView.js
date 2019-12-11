5fca0633177b1b67fcbe8747213272bae0b1df62
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-12-11 05:05:32 +0000

    render results function

---
+++ src/js/views/searchView.js
@@ -0,0 +1,25 @@
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.value;
+
+const renderRecipe = (recipe) => {
+    const markup = `
+        <li>
+            <a class="results__link" href="${recipe.id}">
+                <figure class="results__fig">
+                    <img src="${elements.searchRecipeImage}${recipe.id}-90x90.jpg" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${recipe.title}</h4>
+                    <p class="results__author">The Pioneer Woman</p>
+                </div>
+            </a>
+        </li>
+    `;
+    elements.searchResultList.insertAdjacentHTML('beforeend', markup);
+}
+
+export const renderResults = (recipes) => {
+    // loops throught renderResults and calls renderRecipe on each element of renderResults
+    recipes.forEach(renderRecipe);
+}
