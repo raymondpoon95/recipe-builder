382126ec226e396f17f3c1a6637fdcaaeaccbad7
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-12-13 00:50:15 +0000

    clear input

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,10 @@ import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => {
+    elements.searchInput.value = '';
+}
+
 const renderRecipe = (recipe) => {
     const markup = `
         <li>
